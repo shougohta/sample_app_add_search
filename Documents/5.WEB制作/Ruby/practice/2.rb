37ad5698d0cb333ef55406e142b9783e14aa6ac5
shougohta <kshougo05114405@gmail.com>
2024-01-27 17:21:12 +0900

    test

---
+++ Documents/5.WEB制作/Ruby/practice/2.rb
@@ -0,0 +1,15 @@
+arr = gets.split(" ")
+a1 = arr[0][0,1]
+a2 = arr[0].reverse[0,1]
+
+b1 = arr[1][0,1]
+b2 = arr[1].reverse[0,1]
+
+c1 = arr[2][0,1]
+c2 = arr[2].reverse[0,1]
+
+if a2 == b1 and b2 == c1
+  puts "YES"
+else
+  puts "NO"
+end
